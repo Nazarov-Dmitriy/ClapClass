2cb1e14bb607f18e8e83a822a7971b1fc8552683
unknown <oleg21082001@mail.ru>
2024-08-16 22:48:04 +0300

    добавил компонент articlececooperation

---
+++ src/components/cooperation/ArticleCooperationComponent.vue
@@ -0,0 +1,138 @@
+<template>
+    <section class="article-cooperation">
+        <div class="article-cooperation__container">
+            <div class="article-cooperation__wrapper">
+                <div class="article-cooperation__hero">
+                    <div class="article-cooperation__hero-img-wrapper">
+                        <img
+                            src="../../assets/images/cooperation/article/article-hero-bg.png"
+                            alt=""
+                            class="article-cooperation__hero-img"
+                        />
+                    </div>
+                </div>
+                <div class="article-cooperation__info">
+                    <h2
+                        class="article-cooperation__info-title article-cooperation__info-title--white"
+                    >
+                        Присоединяйтесь
+                    </h2>
+                    <h2
+                        class="article-cooperation__info-title article-cooperation__info-title--orange"
+                    >
+                        к нашей команде!
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.article-cooperation {
+    padding: 48px 64px;
+    box-sizing: border-box;
+    background: url('../../assets/images/cooperation/article/article-bg.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: bottom;
+
+    @media (max-width: $lg) {
+        padding: 48px 16px;
+    }
+    @media (max-width: $sm) {
+        padding: 64px 16px 48px 16px;
+    }
+}
+.article-cooperation__container {
+    max-width: 1312px;
+    margin: 0 auto;
+}
+.article-cooperation__wrapper {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    position: relative;
+    gap: 61px;
+
+    @media (max-width: $lg) {
+        flex-direction: column;
+        gap: 0;
+    }
+}
+.article-cooperation__hero {
+}
+.article-cooperation__hero-img-wrapper {
+}
+.article-cooperation__hero-img {
+    @media (max-width: $sm) {
+        width: 288px;
+    }
+}
+.article-cooperation__info {
+    position: absolute;
+    right: 0;
+
+    @media (max-width: $lg) {
+        display: block;
+        position: static;
+        transform: translate(0, -50px);
+    }
+}
+.article-cooperation__info-title {
+    border: 4px solid #fff;
+    padding: 16px 24px;
+    box-sizing: border-box;
+    box-shadow: 16px 16px 2px 0 rgba(14, 8, 6, 0.15);
+
+    max-width: 606px;
+
+    &--white {
+        border: 4px solid #e05704;
+        padding: 16px 24px;
+        width: 598px;
+        height: 122px;
+        box-shadow: 16px 16px 2px 0 rgba(14, 8, 6, 0.15);
+        background: rgba(255, 255, 255, 0.8);
+        transform: rotate(-6deg);
+        font-family: var(--second-family);
+        font-weight: 700;
+        font-size: 60px;
+        line-height: 150%;
+        text-align: center;
+        color: #e05704;
+        @media (max-width: $lg) {
+            width: 420px;
+            height: 92px;
+            font-size: 40px;
+        }
+        @media (max-width: $sm) {
+            width: 287px;
+            font-size: 26px;
+            height: 71px;
+        }
+    }
+
+    &--orange {
+        background: #e05704;
+        transform: rotate(-2deg);
+        font-family: var(--second-family);
+        font-weight: 700;
+        font-size: 60px;
+        line-height: 150%;
+        text-align: center;
+        color: #fff;
+
+        @media (max-width: $lg) {
+            width: 420px;
+            height: 92px;
+            font-size: 40px;
+        }
+        @media (max-width: $sm) {
+            width: 324px;
+            font-size: 26px;
+            height: 71px;
+        }
+    }
+}
+</style>
---
+++ src/components/cooperation/SupportComponent.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import BtnComponent from '../btns/BtnComponent.vue'
+import BtnComponentWhite from '../btns/BtnComponentWhite.vue'
+</script>
+
+<template>
+    <section class="support">
+        <div class="support__container">
+            <div class="support__wrapper">
+                <div class="support__text-wrapper">
+                    <h2 class="support__title">Поддержите нас</h2>
+                    <p class="support__text">
+                        Расскажите о проекте, внесите вклад в продакшн контента или поддержите
+                        работоспособность сервиса
+                    </p>
+                </div>
+                <div class="support__info">
+                    <div class="support__info-btn-wrapper">
+                        <BtnComponent>Сделайте пожертвование</BtnComponent>
+                        <BtnComponentWhite>Свяжитесь с нами</BtnComponentWhite>
+                    </div>
+
+                    <div class="support__networks-wrapper">
+                        <span>Присоединяйтесь</span>
+                        <img
+                            src="@/assets/icons/footer/youtube.svg"
+                            alt="logo"
+                            class="footer__youtube"
+                        />
+                        <img src="@/assets/icons/footer/vk.svg" alt="logo" class="footer__vk" />
+                        <img
+                            src="@/assets/icons/footer/telegram.svg"
+                            alt="logo"
+                            class="footer__telegram"
+                        />
+                    </div>
+                    <div class="support__share">
+                        <span>Рассказать о нас</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.support {
+    padding: 48px 64px;
+    box-sizing: border-box;
+
+    @media (max-width: 991px) {
+        padding: 48px 16px;
+    }
+    @media (max-width: 767px) {
+        padding: 64px 16px 48px 16px;
+    }
+}
+.support__container {
+    max-width: 1042px;
+    margin: 0 auto;
+}
+.support__wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+}
+
+.support__text-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+.support__title {
+    font-family: 'CenturyGothic';
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 150%;
+    color: $orange;
+    text-align: center;
+}
+.support__text {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 150%;
+    color: $black;
+    text-align: center;
+}
+.support__info {
+    display: grid;
+    justify-content: center;
+    grid-template-columns: auto auto auto;
+    align-items: center;
+    gap: 24px;
+
+    @media (max-width: $lg) {
+        grid-template-columns: 1fr;
+    }
+}
+
+.support__info-btn-wrapper {
+    display: flex;
+    gap: 24px;
+    @media (max-width: $lg) {
+        flex-direction: column;
+        align-items: center;
+    }
+}
+
+.support__networks-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+
+    @media (max-width: $lg) {
+        justify-content: center;
+    }
+}
+
+.support__share {
+    @media (max-width: $xl) {
+        justify-self: center;
+    }
+}
+</style>
