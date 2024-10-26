548888afabc5ae797ada005f0be6d2d4985d0e3a
unknown <oleg21082001@mail.ru>
2024-10-26 16:08:22 +0300

    added problems component

---
+++ src/components/UI/TitleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2 class="title"><slot /></h2>
+</template>
+
+<style lang="scss" scoped>
+.title {
+    font-family: 'CenturyGothic';
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 48px;
+    color: $orange;
+
+    @media (max-width: $lg) {
+        font-size: 32px;
+        line-height: 150%;
+    }
+    @media (max-width: $sm) {
+        font-size: 24px;
+    }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,7 @@ import AchievementComponent from '../components/about/AchievementComponent.vue'
 import AcquaintComponent from '../components/about/AcquaintComponent.vue'
 import ChangeComponent from '../components/about/ChangeComponent.vue'
 import InformationComponent from '../components/about/InformationComponent.vue'
+import ProblemsComponent from '../components/about/ProblemsComponent.vue'
 import SupportComponent from '../components/about/SupportComponent.vue'
 import MainLayout from '../layouts/MainLayout.vue'
 </script>
@@ -12,6 +13,7 @@ import MainLayout from '../layouts/MainLayout.vue'
         <AcquaintComponent />
         <InformationComponent />
         <SupportComponent />
+        <ProblemsComponent />
         <AchievementComponent />
         <ChangeComponent />
     </MainLayout>
