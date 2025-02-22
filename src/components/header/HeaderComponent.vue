46adcb808ee755d1a55a212aec3450c3dab52323
Nazarov-Dmitriy <Difat1@inbox.ru>
2025-02-22 21:52:11 +0300

    add: active header link

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -6,27 +6,47 @@
                     <nav class="header__nav header__nav--mobile">
                         <ul class="header__list">
                             <li class="header__list-item">
-                                <router-link to="/services" class="header__list-link">
+                                <router-link
+                                    active-class="active"
+                                    to="/services"
+                                    class="header__list-link"
+                                >
                                     Сервисы
                                 </router-link>
                             </li>
                             <li class="header__list-item">
-                                <router-link class="header__list-link" to="/about">
+                                <router-link
+                                    active-class="active"
+                                    class="header__list-link"
+                                    to="/about"
+                                >
                                     О проекте
                                 </router-link>
                             </li>
                             <li class="header__list-item">
-                                <router-link to="/cooperation" class="header__list-link">
+                                <router-link
+                                    active-class="active"
+                                    to="/cooperation"
+                                    class="header__list-link"
+                                >
                                     Сотрудничество
                                 </router-link>
                             </li>
                             <li class="header__list-item">
-                                <router-link to="/blog" class="header__list-link">
+                                <router-link
+                                    active-class="active"
+                                    to="/blog"
+                                    class="header__list-link"
+                                >
                                     Блог
                                 </router-link>
                             </li>
                             <li v-if="getUser" class="header__list-item">
-                                <router-link to="/admin" class="header__list-link">
+                                <router-link
+                                    active-class="active"
+                                    to="/admin"
+                                    class="header__list-link"
+                                >
                                     Админстрирование
                                 </router-link>
                             </li>
@@ -67,25 +87,43 @@
                 <nav class="header__nav">
                     <ul class="header__list">
                         <li class="header__list-item">
-                            <router-link to="/services" class="header__list-link">
+                            <router-link
+                                to="/services"
+                                class="header__list-link"
+                                active-class="active"
+                            >
                                 Сервисы
                             </router-link>
                         </li>
                         <li class="header__list-item">
-                            <router-link class="header__list-link" to="/about">
+                            <router-link
+                                class="header__list-link"
+                                to="/about"
+                                active-class="active"
+                            >
                                 О проекте
                             </router-link>
                         </li>
                         <li class="header__list-item">
-                            <router-link to="/cooperation" class="header__list-link">
+                            <router-link
+                                to="/cooperation"
+                                class="header__list-link"
+                                active-class="active"
+                            >
                                 Сотрудничество
                             </router-link>
                         </li>
                         <li class="header__list-item">
-                            <router-link to="/blog" class="header__list-link"> Блог </router-link>
+                            <router-link to="/blog" class="header__list-link" active-class="active">
+                                Блог
+                            </router-link>
                         </li>
                         <li v-if="getUser" class="header__list-item">
-                            <router-link to="/admin" class="header__list-link">
+                            <router-link
+                                to="/admin"
+                                class="header__list-link"
+                                active-class="active"
+                            >
                                 Администрирование
                             </router-link>
                         </li>
@@ -361,7 +399,8 @@ watch(getIsSuccess, (val) => {
         border-bottom: 1px solid $red;
     }
 
-    &:active {
+    &:active,
+    &.active {
         font-family: var(--font-family);
         font-weight: 500;
         font-size: 20px;
